3a53e1a7f217e5c85d193047362dcf2351fd2e35
RuiJin <rui.jin@sjsu.edu>
2020-11-17 16:28:23 -0800

    test

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(proxy(['/api', '/auth'],
+    app.use(proxy(['/api', '/test'],
         { target: 'http://localhost:5000'}
     ))};
